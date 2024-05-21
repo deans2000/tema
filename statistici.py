85b16c21d7559a810af191ea6c08353de9054aee
Slatinat Dean <deanslatinat@yahoo.com>
2024-05-21 18:32:11 +0300

    problema 4

--- statistici.py
+++ statistici.py
@@ -6,4 +6,21 @@ def numarPersoane():
         for row in reader:
             count+=1
         print(f'Sunt {count} persoane inregistrate.')
-numarPersoane()
+# numarPersoane()
+
+def youngestPerson():
+    with open('Curs15/tema/persoane.csv','r',newline='') as csvFile:
+        reader=csv.reader(csvFile)
+        next(reader)
+        firstRow=next(reader)
+        min=int(firstRow[-1])
+        nume=firstRow[0]
+        prenume=firstRow[1]
+        for row in reader:
+            row[-1]=int(row[-1])
+            if min>row[-1]:
+                min=row[-1]
+                nume=row[0]
+                prenume=row[1]
+        print(f'{nume} {prenume} este cel mai tanar, avand {min} ani.')
+youngestPerson()
